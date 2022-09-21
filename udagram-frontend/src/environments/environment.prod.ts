db5ce881e84427303d385224ff4f70a7a3729073
Bright Olawale <oolatunjibright@yahoo.com>
2022-09-21 20:29:52 +0100

    All required files added

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://aa2265f4b5fe24fc89101a11fac012ef-143123469.us-east-2.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://ab60891f8416d4c87898867aaea41c86-1284671510.us-east-2.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
